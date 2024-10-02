5230463f96ad3e39703226d56b47a88fb82b7eb8
The-Lord-of-Owls <rapadantindustries@gmail.com>
2024-10-02 18:40:16 -0500

    Improved some layout stuff for form and cleaned global css file

--- portfolio/src/routes/contact/contact-form.tsx
+++ portfolio/src/routes/contact/contact-form.tsx
@@ -1,6 +1,13 @@
-export default function ContactForm( to: string ) {
+import { FormEvent, ChangeEventHandler, useState } from "react"
 
-	async function handleSubmit(): Promise<void> {
+export default function ContactForm( to: string ) {
+	const [ name, setName ] = useState( "" )
+	const [ email, setEmail ] = useState( "" )
+	const [ message, setMessage ] = useState( "" )
+
+	async function handleSubmit( event: FormEvent<HTMLFormElement> ): Promise<void> {
+		event.preventDefault()
+
 		const res = await fetch( to, {
 			method: "POST"
 		} )
@@ -10,8 +17,25 @@ export default function ContactForm( to: string ) {
 		}
 	}
 
-	return <form className="contact-form">
-		
+	async function handleChange( event: FormEvent<HTMLFormElement> ): Promise<void> {
+		event.preventDefault()
+
+
+	}
+
+
+	return <form className="contact-form" onSubmit={ handleSubmit }>
+		<label>Name:
+			<input type="text" name="name" value={ name } onChange={ handleChange } />
+		</label>
+		<label>Email:
+			<input type="email" name="email" value={ email } onChange={ handleChange } />
+		</label>
+		<label>Message:
+			<input type="text" name="message" value={ message } onChange={ handleChange } />
+		</label>
+
+		<input type="submit" value="Submit" />
 	</form>
 }
 
